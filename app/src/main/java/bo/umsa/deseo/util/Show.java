a0747405af035e34a34443408594b3c2046296f6
maumedmac <maumedmac@gmail.com>
2023-10-10 11:58:54 -0400

    Toda la semana

--- app/src/main/java/bo/umsa/deseo/util/Show.java
+++ app/src/main/java/bo/umsa/deseo/util/Show.java
@@ -1,6 +1,7 @@
 package bo.umsa.deseo.util;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Show {
 
@@ -19,21 +20,127 @@ public class Show {
     public static void initShow()
     {
         ShowArrayList = new ArrayList<>();
+        Calendar c = Calendar.getInstance();
+        //c.set(año,mes,dia) // vairables int
+        int dia =  c.get(Calendar.DAY_OF_WEEK);
+        if(dia==Calendar.SUNDAY){
+            ShowArrayList.add(new Show(0,"07:00 – Música"));
+            ShowArrayList.add(new Show(1,"07:30 – Novilunio"));
+            ShowArrayList.add(new Show(2,"08:30 – Música"));
+            ShowArrayList.add(new Show(3,"09:30 – Luche, Pare de Sufrir"));
+            ShowArrayList.add(new Show(4,"11:30 – Canasta de combate"));
+            ShowArrayList.add(new Show(5,"12:30 – La vuelta al mundo en 60 minutos"));
+            ShowArrayList.add(new Show(6,"13:30 – Música"));
+            ShowArrayList.add(new Show(7,"16:00 – Música"));
+            ShowArrayList.add(new Show(8,"17:00 – Luche, Pare de Sufrir (Reprís)"));
+            ShowArrayList.add(new Show(9,"19:00 – La vuelta al mundo en 60 minutos"));
+            ShowArrayList.add(new Show(10,"20:00 – Plenilunio"));
+            ShowArrayList.add(new Show(11,"21:00 – La Beatleoteca"));
+            ShowArrayList.add(new Show(12,"22:00 – Música"));
+            ShowArrayList.add(new Show(13,"23:00 – Música"));
+            ShowArrayList.add(new Show(14,"24:00 – Cierre de emisión"));
+        }if(dia==Calendar.MONDAY){
         ShowArrayList.add(new Show(0,"07:00 – Música"));
         ShowArrayList.add(new Show(1,"07:30 – Novilunio"));
-        ShowArrayList.add(new Show(1,"08:30 – Música"));
-        ShowArrayList.add(new Show(1,"09:30 – Luche, Pare de Sufrir"));
-        ShowArrayList.add(new Show(1,"11:30 – Canasta de combate"));
-        ShowArrayList.add(new Show(1,"12:30 – La vuelta al mundo en 60 minutos"));
-        ShowArrayList.add(new Show(1,"13:30 – Música"));
-        ShowArrayList.add(new Show(1,"16:00 – Música"));
-        ShowArrayList.add(new Show(1,"17:00 – Luche, Pare de Sufrir (Reprís)"));
-        ShowArrayList.add(new Show(1,"19:00 – La vuelta al mundo en 60 minutos"));
-        ShowArrayList.add(new Show(1,"20:00 – Plenilunio"));
-        ShowArrayList.add(new Show(1,"21:00 – La Beatleoteca"));
-        ShowArrayList.add(new Show(1,"22:00 – Música"));
-        ShowArrayList.add(new Show(1,"23:00 – Música"));
-        ShowArrayList.add(new Show(1,"24:00 – Cierre de emisión"));
+        ShowArrayList.add(new Show(2,"08:30 – Música"));
+        ShowArrayList.add(new Show(3,"09:30 – Luche, Pare de Sufrir"));
+        ShowArrayList.add(new Show(4,"11:30 – Canasta de combate"));
+        ShowArrayList.add(new Show(5,"12:30 – La vuelta al mundo en 60 minutos"));
+        ShowArrayList.add(new Show(6,"13:30 – Música"));
+        ShowArrayList.add(new Show(7,"16:00 – Música"));
+        ShowArrayList.add(new Show(8,"17:00 – Luche, Pare de Sufrir (Reprís)"));
+        ShowArrayList.add(new Show(9,"19:00 – La vuelta al mundo en 60 minutos"));
+        ShowArrayList.add(new Show(10,"20:00 – Plenilunio"));
+        ShowArrayList.add(new Show(11,"21:00 – La Beatleoteca"));
+        ShowArrayList.add(new Show(12,"22:00 – Música"));
+        ShowArrayList.add(new Show(13,"23:00 – Música"));
+        ShowArrayList.add(new Show(14,"24:00 – Cierre de emisión"));
+        }
+        if(dia==Calendar.TUESDAY){
+            ShowArrayList.add(new Show(0,"07:00 – Música"));
+            ShowArrayList.add(new Show(1,"07:30 – Novilunio"));
+            ShowArrayList.add(new Show(2,"08:30 – Música"));
+            ShowArrayList.add(new Show(3,"09:30 – Luche, Pare de Sufrir"));
+            ShowArrayList.add(new Show(4,"11:30 – Canasta de combate"));
+            ShowArrayList.add(new Show(5,"12:30 – La vuelta al mundo en 60 minutos"));
+            ShowArrayList.add(new Show(6,"13:30 – Música"));
+            ShowArrayList.add(new Show(7,"16:00 – Música"));
+            ShowArrayList.add(new Show(8,"17:00 – Luche, Pare de Sufrir (Reprís)"));
+            ShowArrayList.add(new Show(9,"19:00 – La vuelta al mundo en 60 minutos"));
+            ShowArrayList.add(new Show(10,"20:00 – Plenilunio"));
+            ShowArrayList.add(new Show(11,"21:00 – La Beatleoteca"));
+            ShowArrayList.add(new Show(12,"22:00 – Música"));
+            ShowArrayList.add(new Show(13,"23:00 – Música"));
+            ShowArrayList.add(new Show(14,"24:00 – Cierre de emisión"));
+        }
+        if(dia==Calendar.WEDNESDAY){
+            ShowArrayList.add(new Show(0,"07:00 – Música"));
+            ShowArrayList.add(new Show(1,"07:30 – Novilunio"));
+            ShowArrayList.add(new Show(2,"08:30 – Música"));
+            ShowArrayList.add(new Show(3,"09:30 – Luche, Pare de Sufrir"));
+            ShowArrayList.add(new Show(4,"11:30 – Canasta de combate"));
+            ShowArrayList.add(new Show(5,"12:30 – La vuelta al mundo en 60 minutos"));
+            ShowArrayList.add(new Show(6,"13:30 – Música"));
+            ShowArrayList.add(new Show(7,"16:00 – Música"));
+            ShowArrayList.add(new Show(8,"17:00 – Luche, Pare de Sufrir (Reprís)"));
+            ShowArrayList.add(new Show(9,"19:00 – La vuelta al mundo en 60 minutos"));
+            ShowArrayList.add(new Show(10,"20:00 – Plenilunio"));
+            ShowArrayList.add(new Show(11,"21:00 – La Beatleoteca"));
+            ShowArrayList.add(new Show(12,"22:00 – Música"));
+            ShowArrayList.add(new Show(13,"23:00 – Música"));
+            ShowArrayList.add(new Show(14,"24:00 – Cierre de emisión"));
+        }
+        if(dia==Calendar.THURSDAY){
+            ShowArrayList.add(new Show(0,"07:00 – Música"));
+            ShowArrayList.add(new Show(1,"07:30 – Novilunio"));
+            ShowArrayList.add(new Show(2,"08:30 – Música"));
+            ShowArrayList.add(new Show(3,"09:30 – Luche, Pare de Sufrir"));
+            ShowArrayList.add(new Show(4,"11:30 – Canasta de combate"));
+            ShowArrayList.add(new Show(5,"12:30 – La vuelta al mundo en 60 minutos"));
+            ShowArrayList.add(new Show(6,"13:30 – Música"));
+            ShowArrayList.add(new Show(7,"16:00 – Música"));
+            ShowArrayList.add(new Show(8,"17:00 – Luche, Pare de Sufrir (Reprís)"));
+            ShowArrayList.add(new Show(9,"19:00 – La vuelta al mundo en 60 minutos"));
+            ShowArrayList.add(new Show(10,"20:00 – Plenilunio"));
+            ShowArrayList.add(new Show(11,"21:00 – La Beatleoteca"));
+            ShowArrayList.add(new Show(12,"22:00 – Música"));
+            ShowArrayList.add(new Show(13,"23:00 – Música"));
+            ShowArrayList.add(new Show(14,"24:00 – Cierre de emisión"));
+        }
+        if(dia==Calendar.FRIDAY){
+            ShowArrayList.add(new Show(0,"07:00 – Música"));
+            ShowArrayList.add(new Show(1,"07:30 – Novilunio"));
+            ShowArrayList.add(new Show(2,"08:30 – Música"));
+            ShowArrayList.add(new Show(3,"09:30 – Luche, Pare de Sufrir"));
+            ShowArrayList.add(new Show(4,"11:30 – Canasta de combate"));
+            ShowArrayList.add(new Show(5,"12:30 – La vuelta al mundo en 60 minutos"));
+            ShowArrayList.add(new Show(6,"13:30 – Música"));
+            ShowArrayList.add(new Show(7,"16:00 – Música"));
+            ShowArrayList.add(new Show(8,"17:00 – Luche, Pare de Sufrir (Reprís)"));
+            ShowArrayList.add(new Show(9,"19:00 – La vuelta al mundo en 60 minutos"));
+            ShowArrayList.add(new Show(10,"20:00 – Plenilunio"));
+            ShowArrayList.add(new Show(11,"21:00 – La Beatleoteca"));
+            ShowArrayList.add(new Show(12,"22:00 – Música"));
+            ShowArrayList.add(new Show(13,"23:00 – Música"));
+            ShowArrayList.add(new Show(14,"24:00 – Cierre de emisión"));
+        }
+        if(dia==Calendar.SATURDAY){
+            ShowArrayList.add(new Show(0,"07:00 – Música"));
+            ShowArrayList.add(new Show(1,"07:30 – Novilunio"));
+            ShowArrayList.add(new Show(2,"08:30 – Música"));
+            ShowArrayList.add(new Show(3,"09:30 – Luche, Pare de Sufrir"));
+            ShowArrayList.add(new Show(4,"11:30 – Canasta de combate"));
+            ShowArrayList.add(new Show(5,"12:30 – La vuelta al mundo en 60 minutos"));
+            ShowArrayList.add(new Show(6,"13:30 – Música"));
+            ShowArrayList.add(new Show(7,"16:00 – Música"));
+            ShowArrayList.add(new Show(8,"17:00 – Luche, Pare de Sufrir (Reprís)"));
+            ShowArrayList.add(new Show(9,"19:00 – La vuelta al mundo en 60 minutos"));
+            ShowArrayList.add(new Show(10,"20:00 – Plenilunio"));
+            ShowArrayList.add(new Show(11,"21:00 – La Beatleoteca"));
+            ShowArrayList.add(new Show(12,"22:00 – Música"));
+            ShowArrayList.add(new Show(13,"23:00 – Música"));
+            ShowArrayList.add(new Show(14,"24:00 – Cierre de emisión"));
+        }
 
     }
 
